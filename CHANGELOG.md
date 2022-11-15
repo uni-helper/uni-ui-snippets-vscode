12426c6abd4a458bfe86e231c8e25a740fbac7a0
ModyQyW <wurui-dev@foxmail.com>
2022-11-15 11:52:09 +0800

    chore: rebrand

---
+++ CHANGELOG.md
@@ -0,0 +1,80 @@
+# 改动日志
+
+## 0.6.5
+
+- 切换到 [uni-helper](https://github.com/uni-helper) 组织下
+
+## 0.6.4
+
+- 发布流程改进
+- 更新链接
+
+## 0.6.3
+
+- 发布流程改进
+
+## 0.6.2
+
+- 修复文档部分错误
+
+## 0.6.1
+
+- 增加适用的 `language`
+
+## 0.6.0
+
+- 增加了 `prefix`，现在有更多的触发方式
+  - `["uni-badge", "<uni-badge>"]` -> `["uni-badge", "<uni-badge>", "UniBadge", "<UniBadge>"]`
+  - 为了与 `uview-ui` 区分，没有添加和 `uBadge` 类似的 `prefix`
+- 移除了没有在 [仓库 README](https://github.com/dcloudio/uni-ui) 和 [官网](https://uniapp.dcloud.io/component/uniui/uni-ui) 列出来的组件
+- 根据示例调整了部分 `body`
+- 修复 README 链接
+
+## 0.5.1
+
+- 修复 README 链接
+
+## 0.5.0
+
+- 添加 `uni-table`，`uni-th`，`uni-tr` 和 `uni-td`
+- 修复 README 链接
+
+## 0.4.1
+
+- 用脚本生成 README
+
+## 0.4.0
+
+- 增加 `uni-data-menu` 和 `uni-file-picker`
+- 更新文档说明
+
+## 0.3.0
+
+- 使用 `node` 生成 `README.md`
+- 增加 `uni-data-picker`
+- 更新部分代码块
+- 更新文档说明
+
+## 0.2.0
+
+- 移除不符合 Vue.js 风格指南的代码块
+- 更新部分代码块
+- 更新所有信息链接
+- 增加 uni-row，uni-col 和 uni-section
+- 更新文档说明
+
+## 0.1.2
+
+- 移除不符合 `Vue.js` 风格指南的代码块
+- 更新文档说明
+
+## 0.1.1
+
+- 更新文档说明
+
+## 0.1.0
+
+- `uni-ui` 组件代码片段
+- 支持 vue2
+- 参考 [uni-ui 文档](https://github.com/dcloudio/uni-ui#readme)
+- 参考 [Vue.js 风格指南](https://cn.vuejs.org/v2/style-guide/index.html)
---
+++ scripts/generate.ts
@@ -0,0 +1,71 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = resolve(__dirname, '..');
+
+const htmlObject = JSON.parse(
+  readFileSync(resolve(root, 'snippets', 'vue-html.json'), { encoding: 'utf8' }),
+);
+
+let readme = `# @uni-helper/uni-ui-snippets-vscode
+
+[![License](https://img.shields.io/github/license/uni-helper/uni-ui-snippets-vscode)](https://github.com/uni-helper/uni-ui-snippets-vscode/blob/main/LICENSE)
+
+[![VSCode](https://vsmarketplacebadge.apphb.com/version-short/uni-helper.uni-ui-snippets-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=uni-helper.uni-ui-snippets-vscode)
+
+[![OpenVSX](https://img.shields.io/badge/dynamic/json?color=brightgreen&label=OpenVSX&query=%24.version&url=https%3A%2F%2Fopen-vsx.org%2Fapi%2Funi-helper%2Funi-ui-snippets-vscode)](https://open-vsx.org/extension/uni-helper/uni-ui-snippets-vscode)
+
+[改动日志](https://github.com/uni-helper/uni-ui-snippets-vscode/blob/main/CHANGELOG.md)
+
+## 插件特性
+
+- uni-ui 基本能力代码片段
+- 参考 [uni-ui 官方组件文档](https://github.com/dcloudio/uni-ui#readme)
+- 参考 [Vue.js 2 风格指南](https://v2.cn.vuejs.org/v2/style-guide/) 和 [Vue.js 3 风格指南](https://cn.vuejs.org/style-guide/)
+
+**插件和文档的冲突之处，请以文档为准。**
+
+插件源代码在 [uni-helper/uni-ui-snippets-vscode](https://github.com/uni-helper/uni-ui-snippets-vscode)。欢迎提交 ISSUE 和 PR 改进本插件。
+
+## 使用
+
+安装插件后重启 VSCode 即可。
+
+`;
+
+// 添加 HTML
+readme += '## HTML\n\n';
+readme += '|API|Prefix|Description|\n|-|-|-|\n';
+for (const key of Object.keys(htmlObject)) {
+  const { prefix, body, description } = htmlObject[key];
+  let newPrefix = '';
+  for (const text of prefix) {
+    newPrefix += `\`${text}\`, `;
+  }
+  newPrefix = newPrefix.slice(0, -2);
+  let newBody = '';
+  newBody = `\`${body[0]
+    .replace(/\b .*[/>]/g, '')
+    .replace(/\(?\([\w "$'(),/:=>{|}]+/g, '()')
+    .replace(/\$\d[\w/<>-]*/g, '')}`;
+  if (newBody.includes('/* ') && !newBody.includes(' */')) {
+    newBody += ' */`';
+  } else if (newBody.includes('<!-- ') && !newBody.includes(' -->')) {
+    newBody += ' -->`';
+  } else if (newBody.includes('<') && !newBody.includes('>')) {
+    newBody += '>`';
+  } else {
+    newBody += '`';
+  }
+  readme += `|${newBody}|${newPrefix}|${description}|\n`;
+}
+readme += '\n';
+
+readme += `## 额外推荐
+
+请查看 [uni-helper 插件说明](https://marketplace.visualstudio.com/items?itemName=uni-helper.uni-helper-vscode)。
+`;
+
+writeFileSync(resolve(root, 'README.md'), readme);
